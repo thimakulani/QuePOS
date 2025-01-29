4baa0ff147cbe8cc386cc2448863628592bf0ad3
Thima <Thimakulani@gmail.com>
2025-01-29 17:01:42 +0200

    seeding user

--- QuePOS.API/Services/SeedService.cs
+++ QuePOS.API/Services/SeedService.cs
@@ -1,27 +1,42 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using QuePOS.API.Models;
 
 namespace QuePOS.API.Services
 {
     public class SeedService
     {
-            private readonly RoleManager<IdentityRole> _roleManager;
-            public async Task SeedAsync()
+        private UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public async Task SeedAsync()
+        {
+            string[] roles = { "Admin", "Store Owner", "Store Employee" };
+            foreach (var roleName in roles)
             {
-                string[] roles = { "Admin", "Store Owner", "Store Employee" };
-                foreach (var roleName in roles)
+                if (!await _roleManager.RoleExistsAsync(roleName))
                 {
-                    if (!await _roleManager.RoleExistsAsync(roleName))
-                    {
-                        await _roleManager.CreateAsync(new IdentityRole(roleName));
-                    }
+                    await _roleManager.CreateAsync(new IdentityRole(roleName));
                 }
             }
-
-            public SeedService(RoleManager<IdentityRole> roleManager)
-            {
-                _roleManager = roleManager;
-            }
-
-
         }
+        public void SeedAdmin()
+        {
+            StoreUser storeUser = new StoreUser()
+            {
+                CreatedAt = DateTime.UtcNow,
+                Email = "thimakulani@gmail.com",
+                FirstName = "Thima",
+                LastName = "Sigauque",
+                Password = "LUna@123",
+                PhoneNumber = "0713934923",
+                
+            };
+        }
+        public SeedService(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            this.userManager = userManager;
+        }
+
+
+    }
 }
