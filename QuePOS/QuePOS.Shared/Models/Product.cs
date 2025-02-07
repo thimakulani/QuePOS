911d3cce5bbe839c5028b1762fe1e31addd7f413
Thima SigauQue <thimakulani@gmail.com>
2025-02-07 07:51:16 +0200

    products

--- QuePOS/QuePOS.Shared/Models/Product.cs
+++ QuePOS/QuePOS.Shared/Models/Product.cs
@@ -5,6 +5,8 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public string Base64Url { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public int StockQuantity { get; set; }
     public int CategoryID { get; set; }
