a4fba4c07a6fb99400fe99309dd385cae82fbdb6
Thima <Thimakulani@gmail.com>
2025-02-17 08:53:34 +0200

    *

--- QuePOS/QuePOS.Shared/Models/Product.cs
+++ QuePOS/QuePOS.Shared/Models/Product.cs
@@ -10,6 +10,7 @@ public class Product
     public string Base64Url { get; set; } = string.Empty;
     public double? Price { get; set; }
     public int? StockQuantity { get; set; }
+    public bool IsDeleted { get; set; } 
     public int CategoryID { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 
